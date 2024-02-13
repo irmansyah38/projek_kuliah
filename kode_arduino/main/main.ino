1784d1180f2e3755d7b05f02c10906a391e96a66
irmansyah38 <irmansya146@gmail.com>
2024-02-13 13:52:50 +0700

    hapus folder

--- kode_arduino/main/main.ino
+++ kode_arduino/main/main.ino
@@ -1,14 +1,15 @@
 #include "fungsi.h"
 #include "suhu.h"
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
   setupSuhu();
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   suhu();
 }
